5a76dc4a26e124aaf2d36d02ceba29de5547d6dc
sambodanis <sam.bodanis@gmail.com>
2014-05-27 20:41:22 +0100

    Initial commit

---
+++ server/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask.ext.mongoengine import MongoEngine
+import json
+
+settings_path = 'config/settings.json'
+settings = json.loads(open(settings_path, 'r').read())
+db_settings = settings['db_settings']
+
+app = Flask(__name__)
+app.config["MONGODB_SETTINGS"] = {"DB": db_settings['DB']}
+app.config["SECRET_KEY"] = db_settings["SECRET"]
+
+db = MongoEngine(app)
+
+if __name__ == '__main__':
+    app.run()
