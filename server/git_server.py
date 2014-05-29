23397e7f9aa9e7943d84810f1d4265a881077d1c
sambodanis <sam.bodanis@gmail.com>
2014-05-29 19:54:31 +0100

    test

--- server/git_server.py
+++ server/git_server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def pull():
-    return 'Hello World!'
+    return 'Hello World!a'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',
